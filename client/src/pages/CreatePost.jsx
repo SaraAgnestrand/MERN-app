b6bae8b93ae7a1e102088cc11141c41a23bd1068
Sara Agnestrand <sara@agnestrand.net>
2023-11-13 11:57:37 +0100

    More code

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -43,12 +43,35 @@ const CreatePost = () => {
     }
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    if(form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(form)
+        })
+
+        await response.json();
+        navigate('/');
+      } catch (error) {
+        alert(err)
+      }finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please generate an image with proper details');
+    }
+  };
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
   }
-
-  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
-
 
   const handleSurpriseMe = () => {
     const randomPrompt = getRandomPrompt(form.prompt);
--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -17,23 +17,23 @@ const config = new OpenAI({
     res.send('Hello from DALL-E!');
   })
 
-  router.route('/').post(async (req, res)=> {
-    try{
-       const { prompt } = req.body;  
-
-       const aiResponse = await openai.images.generate({
+  router.route('/').post(async (req, res) => {
+    try {
+      const { prompt } = req.body;
+  
+      const aiResponse = await openai.images.generate({
         prompt,
         n: 1,
-        size: "1024x1024",
-        response_format: "b64_json",
-      });
+        size: '1024x1024',
+        response_format: 'b64_json',
+      });  
   
       const image = aiResponse.data[0].b64_json;
       res.status(200).json({ photo: image });
-    } catch(error) {
-        console.log(error);
-        res.status(500).send(error?.response.data.error.message)
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error?.response.data.error.message || 'Something went wrong');
     }
-  })
+  });
   
 export default router;
